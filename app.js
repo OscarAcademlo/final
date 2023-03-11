c42ab9cc2f187d18a1d5168529086be62223a98a
OscarAcademlo <oscarns@gmail.com>
2023-03-11 11:34:25 -0300

    data 1103

--- app.js
+++ app.js
@@ -4,10 +4,12 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+
 require('dotenv').config();
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var loginRouter = require('./routes/admin/login')
 
 var app = express();
 
@@ -23,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/admin/login',loginRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
