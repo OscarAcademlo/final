fb353a299179c51ef314b61cb8d3de560a89b645
OscarAScademlo <oscarns@gmail.com>
2023-03-23 07:11:18 -0300

    subiendo con error

--- routes/admin/novedades.js
+++ routes/admin/novedades.js
@@ -1,13 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var novedadesModels = require('../../models/novedadesModels')
 
-router.get('/', function (req, res, next) {
+// get novedades
+router.get('/', async function (req, res, next)  {
+    var novedades = await novedadesModels.getNovedades();
     res.render('admin/novedades', {
         layout: 'admin/layout',
-        persona: req.session.nombre
+        persona: req.session.nombre,
+        novedades
+        
     });
 });
 
-
+// delete novedades
+router.get('/eliminar/:id', async (req, res, next) =>  {
+    const id = req.params.id;
+    await novedadesModels.deleteNovedadesById(id);
+    res.redirect('/admin/novedades')
+    
+    });
 
 module.exports = router;
