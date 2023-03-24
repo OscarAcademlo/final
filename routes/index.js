468f0c62465ea69fb84ea0acfe8f4183c38f4214
OscarAcademlo <oscarns@gmail.com>
2023-03-24 16:19:32 -0300

    novedades

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,14 @@
 var express = require('express');
 var router = express.Router();
 var nodemailer = require('nodemailer');
+var novedadesModels = require('../models/novedadesModels')
+
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', async function(req, res, next) {
+var novedades = await novedadesModels.getNovedades();
+
+  res.render('index', {novedades});
 });
 
 router.post('/',async(req,res,next) =>{
